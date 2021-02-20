7fde2daa17a7d8d083605c7e650631a0ef06b78c
JinOhKwon <kjo1333@gmail.com>
2021-02-20 14:29:51 +0900

    useCallback, memo 등록

--- src/components/Lotto.tsx
+++ src/components/Lotto.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Grid, List } from '@material-ui/core';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import Ball from './Ball';
 
 /**
@@ -22,9 +22,15 @@ function getLottoNumbers(): Array<number> {
 
 function Lotto() {
     /**
+     * 로도 번호 메모 목록
+     * 
+     * @TODO 리액트 훅 메모 사용법 확인 필요
+     */
+    const lottoNumberMemos = useMemo(() => getLottoNumbers(), []);
+    /**
      * 로또 번호
      */
-    const [lottoNumbers, setLottoNumbers] = useState(getLottoNumbers());
+    const [lottoNumbers, setLottoNumbers] = useState(lottoNumberMemos);
     /**
      * 로또 공
      */
@@ -45,27 +51,27 @@ function Lotto() {
     /**
      * 초기화
      */
-    const onResetNumber = () => {
+    const onResetNumber = useCallback(() => {  // 초기화
         setLottoNumbers(getLottoNumbers());
         setLottoBalls([]);
         setBonus(null);
         setIsRedo(false);
         timeouts.current = [];
-    };
+    }, [lottoNumbers]);
 
     /**
      * 리액트 hook
      */
     useEffect(() => {
-         // 1초 간격으로 출력
-        for(let i = 0 ; i < lottoNumbers.length - 1; i++){
+        // 1초 간격으로 출력
+        for (let i = 0; i < lottoNumbers.length - 1; i++) {
             timeouts.current[i] = setTimeout(() => {
                 setLottoBalls((prevBalls: any) => [...prevBalls, lottoNumbers[i]]);
             }, (i + 1) * 1000);
         }
 
         // 보너스공
-        timeouts.current[6] = setTimeout(() => { 
+        timeouts.current[6] = setTimeout(() => {
             setBonus(lottoNumbers[1] as any);
             setIsRedo(true);
         }, 7000);
@@ -97,7 +103,7 @@ function Lotto() {
                         결과창
                     </Box>
                     <List>
-                        {lottoBalls.map((winBall: any, idx: any) => <Ball key={idx} lottoNumber={winBall}/>)}
+                        {lottoBalls.map((winBall: any, idx: any) => <Ball key={idx} lottoNumber={winBall} />)}
                     </List>
                 </Grid>
 
@@ -105,7 +111,7 @@ function Lotto() {
                     <Box component='h3'>
                         보너스
                     </Box>
-                    {bonus && <Ball lottoNumber={bonus}/>}
+                    {bonus && <Ball lottoNumber={bonus} />}
                     {isRedo && <Button onClick={onResetNumber}>한 번 더!</Button>}
                 </Grid>
             </Grid>
